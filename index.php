624ed81d85e426a9f29ad6b421f4196c7ff2cdc6
meysamabs <166728395+meysamabs@users.noreply.github.com>
2025-05-07 01:38:21 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
-
-$name = $_GET['name'] ?? $_POST['name'] ?? 'مهمان';
-echo json_encode(["message" => "سلام، $name!"]);
+echo 'meysam abbaspour';
 ?>
